d5f43817d1b29ce6f84593357863cff08e5540c5
leo <sebawar@126.com>
2018-06-22 16:58:42 +0800

    add docker build

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+docker kill $(docker ps -q)
+
+docker rm $(docker ps -a -q)
+
+docker rmi $(docker images -q)
+
+docker build -t slack-flask .
+
+docker run -d --name slack-flask -p 80:80 slack-flask
