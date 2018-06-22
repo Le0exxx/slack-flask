5760ac828d22b332dce4b709a7a44c3aacafbbf4
leo <sebawar@126.com>
2018-06-22 17:29:57 +0800

    add tag slack

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@ docker kill $(docker ps -q)
 
 docker rm $(docker ps -a -q)
 
-docker rmi $(docker images --filter=reference="*:slack*" -q)
+docker rmi $(docker images --filter=reference="*:latest*" -q)
 
-docker build -t slack-flask:slack .
+docker build -t slack-flask .
 
-docker run -d --name slack-flask:slack -p 80:80 slack-flask
+docker run -d --name slack-flask -p 80:80 slack-flask
